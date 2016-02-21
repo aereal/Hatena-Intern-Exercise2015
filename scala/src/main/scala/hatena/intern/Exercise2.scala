30093f0b838ff38f7f962e8938b7a47c15b7de6d
aereal <aereal@aereal.org>
2016-02-21 18:56:27 +0900

    2

--- scala/src/main/scala/hatena/intern/Exercise2.scala
+++ scala/src/main/scala/hatena/intern/Exercise2.scala
@@ -2,6 +2,38 @@ package hatena.intern
 
 import scalax.file.Path
 
+object LTSVLine {
+  def unapply(line: String): Option[Log] = {
+    val pairs = for (records <- line.split("\t")) yield records.split(":", 2)
+    val records = pairs.map { case Array(k, v) => (k, v) }.toMap
+    for (
+      host <- records.get("host");
+      user <- records.get("user");
+      req <- records.get("req");
+      referer <- records.get("referer");
+      status <- records.get("status");
+      statusInt <- safeStringToInt(status);
+      size <- records.get("size");
+      sizeInt <- safeStringToInt(size);
+      epoch <- records.get("epoch");
+      epochInt <- safeStringToInt(epoch)
+    ) yield Log(host, user, epochInt, req, statusInt, sizeInt, referer)
+  }
+
+  def safeStringToInt(s: String): Option[Int] = {
+    import scala.util.control.Exception._
+    catching(classOf[NumberFormatException]).opt(s.toInt)
+  }
+}
+
 object LtsvParser {
-  def parse(filePath: String): Iterable[Log] = ???
+  def safeParse(filePath: String): Iterable[Option[Log]] = {
+    Path.fromString(filePath).lines().map {
+      case LTSVLine(log) => Some(log)
+    }.toIterable
+  }
+
+  def parse(filePath: String): Iterable[Log] = {
+    safeParse(filePath).map(_.get)
+  }
 }
--- scala/src/test/scala/hatena/intern/Exercise2.scala
+++ scala/src/test/scala/hatena/intern/Exercise2.scala
@@ -6,10 +6,14 @@ class Exercise2Spec extends UnitSpec {
 
   describe("LTSV Parser") {
     it("LTSVファイルが正しくパースされていること") {
-      val logs = LtsvParser.parse("/path/to/sample_data/log.ltsv") // リポジトリ内の`sample_data/log.ltsv`へのパスを指定してください
+      val logs = LtsvParser.parse("/Users/aereal/devel/src/github.com/hatena/Hatena-Intern-Exercise2015/sample_data/log.ltsv") // リポジトリ内の`sample_data/log.ltsv`へのパスを指定してください
       logs.size shouldBe 5
 
-      // 以降ファイルが正しくLogクラスにパースされているテストを書いてみてください
+      val logsList = logs.toList
+      val firstLog = logsList(0)
+      firstLog.host shouldBe "127.0.0.1"
+      firstLog.user shouldBe "frank"
+      firstLog.referer shouldBe "http://www.hatena.ne.jp/"
     }
 
     it("LTSVファイルが正しくパースできない形式の場合") {
