4cddcc570a5b8a88f08a59dd1f56897da91cb24a
aereal <aereal@aereal.org>
2016-02-21 18:49:41 +0900

    1

--- scala/src/main/scala/hatena/intern/Exercise1.scala
+++ scala/src/main/scala/hatena/intern/Exercise1.scala
@@ -1,9 +1,17 @@
 package hatena.intern
 
+import org.joda.time.{ DateTime, DateTimeZone }
+import org.joda.time.format.{ DateTimeFormat }
+
 case class Log(host: String, user: String, epoch: Int, req: String, status: Int, size: Int, referer: String) {
-  def method: String = ???
-  def path: String = ???
-  def protocol: String = ???
-  def uri: String = ???
-  def time: String = ???
+  private val Array(reqMethod, reqPath, reqProtocol) = req.split(' ')
+  def method: String = reqMethod
+  def path: String = reqPath
+  def protocol: String = reqProtocol
+  def uri: String = "http://" ++ host ++ path
+  def time: String = {
+    val d = new DateTime(epoch.toLong * 1000).withZone(DateTimeZone.UTC)
+    val fmt = DateTimeFormat.forPattern("YYYY-MM-dd'T'HH:mm:ss")
+    fmt.print(d)
+  }
 }
